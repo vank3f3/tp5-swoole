c62230a2d5a5e6755e6b4802c30e04842d2424ff
cqiyu <730362162@qq.com>
2019-12-19 21:33:54 +0800

    supplement swoole task

---
+++ src/core/Invoker.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace william\swoole\core;
+
+
+class Invoker
+{
+    public static function callUserFunc(callable $callable,...$params)
+    {
+        if(SWOOLE_VERSION >1){
+            if($callable instanceof \Closure){
+                return $callable(...$params);
+            }else if(is_array($callable) && is_object($callable[0])){
+                $class = $callable[0];
+                $method = $callable[1];
+                return $class->$method(...$params);
+            }else if(is_array($callable) && is_string($callable[0])){
+                $class = $callable[0];
+                $method = $callable[1];
+                return $class::$method(...$params);
+            }else if(is_string($callable)){
+                return $callable(...$params);
+            }else{
+                return null;
+            }
+        }else{
+            return call_user_func($callable,...$params);
+        }
+    }
+
+    public static function callUserFuncArray(callable $callable,array $params)
+    {
+        if(SWOOLE_VERSION > 1){
+            if($callable instanceof \Closure){
+                return $callable(...$params);
+            }else if(is_array($callable) && is_object($callable[0])){
+                $class = $callable[0];
+                $method = $callable[1];
+                return $class->$method(...$params);
+            }else if(is_array($callable) && is_string($callable[0])){
+                $class = $callable[0];
+                $method = $callable[1];
+                return $class::$method(...$params);
+            }else if(is_string($callable)){
+                return $callable(...$params);
+            }else{
+                return null;
+            }
+        }else{
+            return call_user_func_array($callable,$params);
+        }
+    }
+}
